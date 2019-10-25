ba4c4cb72900b25bfa76dfcf16e2c6f7ae3d9f00
Antoine CELLIER <antoine.cellier@zenika.com>
2019-10-25 11:08:22 +0200

    :sparkles: Init recipes context

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import HomePage from "./pages/home";
 import RecipeDetailPage from "./pages/recipeDetail";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { RecipesProvider } from "./context/recipes/index"
 import "./styles.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/recipes/:id" component={RecipeDetailPage} />
-        <Route component={({ location }) => (
-            <div className="route-not-found">
-              The page <code>{location.pathname}</code> could not be found.
-            </div>
-          )}
-        />
-      </Switch>
+      <RecipesProvider>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/recipes/:id" component={RecipeDetailPage} />
+          <Route component={({ location }) => (
+              <div className="route-not-found">
+                The page <code>{location.pathname}</code> could not be found.
+              </div>
+            )}
+          />
+        </Switch>
+      </RecipesProvider>
     </BrowserRouter>
   );
 }
