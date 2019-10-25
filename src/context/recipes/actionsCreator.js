590a8bd85e3894c1c25e585776166358f86e5d89
Antoine CELLIER <antoine.cellier@zenika.com>
2019-10-25 10:28:13 +0200

    :seedling: Bootstrap context files import

---
+++ src/context/recipes/actionsCreator.js
@@ -0,0 +1,5 @@
+export const actions = {}
+
+const actionsCreator = (dispatch) => ({})
+
+export default actionsCreator
---
+++ src/context/recipes/index.js
@@ -0,0 +1,2 @@
+import React, { useReducer, createContext, useContext } from 'react'
+import actionsCreator, { actions } from './actionsCreator'
