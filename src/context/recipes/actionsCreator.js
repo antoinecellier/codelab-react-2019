32432707ff1e9ee631221d69985970d605f76726
Antoine CELLIER <antoine.cellier@zenika.com>
2019-11-04 15:11:06 +0100

    ♻️ rename variable sideActions

--- src/context/recipes/actionsCreator.js
+++ src/context/recipes/actionsCreator.js
@@ -3,33 +3,35 @@ export const sideActions = {
     CURRENT_LOADED: 'CURRENT_LOADED',
 }
 
+const lastName = '<votre nom>'
+
 const actionsCreator = (dispatch) => ({
     list: async () => {
-        const response = await fetch('http://react-19-20.cleverapps.io/antoine/recettes')
+        const response = await fetch(`http://react-19-20.cleverapps.io/${lastName}/recettes`)
         const list = await response.json()
-        return dispatch({
+        dispatch({
             type: sideActions.LIST_LOADED,
             payload: list,
         })
     },
     get: async (id) => {
-        const response = await fetch(`http://react-19-20.cleverapps.io/antoine/recettes/${id}`)
+        const response = await fetch(`http://react-19-20.cleverapps.io/${lastName}/recettes/${id}`)
         const recipe = await response.json()
-        return dispatch({
+        dispatch({
             type: sideActions.CURRENT_LOADED,
             payload: recipe,
         })
     },
     post: async (recipe) => {
         const response = await fetch(
-            `http://react-19-20.cleverapps.io/antoine/recettes/${recipe.id}`, 
+            `http://react-19-20.cleverapps.io/${lastName}/recettes/${recipe.id}`, 
             {
                 method: 'PUT',
                 body: JSON.stringify(recipe),
                 headers: { 'content-type': 'application/json' }
             })
         const recipeUploaded = await response.json()
-        return dispatch({
+        dispatch({
             type: sideActions.CURRENT_LOADED,
             payload: recipeUploaded,
         })
