b7a9cc1261309a59a12d3d094c70f6aa6fb5a76a
Antoine CELLIER <antoine.cellier@zenika.com>
2019-11-04 15:59:36 +0100

    ♻️ add lastName for rest service

--- src/context/ingredients/actionsCreator.js
+++ src/context/ingredients/actionsCreator.js
@@ -2,11 +2,13 @@ export const sideActions = {
     LIST_LOADED: 'LIST_LOADED',
 }
 
+const lastName = '<votre nom>'
+
 const actionsCreator = (dispatch) => ({
     list: async () => {
-        const response = await fetch('http://react-19-20.cleverapps.io/antoine/ingredients')
+        const response = await fetch(`http://react-19-20.cleverapps.io/${lastName}/ingredients`)
         const list = await response.json()
-        return dispatch({
+        dispatch({
             type: sideActions.LIST_LOADED,
             payload: list,
         })
